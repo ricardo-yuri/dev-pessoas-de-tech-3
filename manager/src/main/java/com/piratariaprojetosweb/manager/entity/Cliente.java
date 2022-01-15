a7615397fd598f0ddc6ae9e1a84ac046f15f29d6
ricardo-yuri <ricardo-yuri@outlook.com>
2022-01-15 20:05:02 -0300

    Entidade Cliente

---
+++ manager/src/main/java/com/piratariaprojetosweb/manager/entity/Cliente.java
@@ -0,0 +1,51 @@
+package com.piratariaprojetosweb.manager.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "tb_cliente")
+public class Cliente implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotEmpty(message = "Campo Nome obrigatório")
+	private String nome;
+
+	private String email;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
