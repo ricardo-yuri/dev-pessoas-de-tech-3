9591ace9f07020a0e363d980c18b5dcbdb0bd2ae
ricardo-yuri <ricardo-yuri@outlook.com>
2022-01-23 10:13:36 -0300

    Relacionamentos

--- manager/src/main/java/com/piratariaprojetosweb/manager/entity/Funcionario.java
+++ manager/src/main/java/com/piratariaprojetosweb/manager/entity/Funcionario.java
@@ -3,27 +3,13 @@ package com.piratariaprojetosweb.manager.entity;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity(name = "tb_funcionario")
 public class Funcionario extends Pessoa implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	private String cargo; // TODO CRIAR ENUM CARGO
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
 
 	public String getCargo() {
 		return cargo;
--- manager/src/main/java/com/piratariaprojetosweb/manager/entity/Pessoa.java
+++ manager/src/main/java/com/piratariaprojetosweb/manager/entity/Pessoa.java
@@ -3,6 +3,7 @@ package com.piratariaprojetosweb.manager.entity;
 import java.io.Serializable;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,14 +11,16 @@ import javax.persistence.MappedSuperclass;
 import javax.persistence.OneToOne;
 
 @MappedSuperclass
-public class Pessoa implements Serializable {
+public abstract class Pessoa implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column
 	private String nome;
+	
 	@OneToOne(cascade = CascadeType.PERSIST)
 	private Contato contato;
 
