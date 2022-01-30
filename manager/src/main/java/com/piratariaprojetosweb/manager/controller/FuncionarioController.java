09e417e3edb1e1b573c2723a98ac45ce60486e69
ricardo-yuri <ricardo-yuri@outlook.com>
2022-01-30 10:42:40 -0300

    Inicializando config spring security

--- manager/src/main/java/com/piratariaprojetosweb/manager/controller/FuncionarioController.java
+++ manager/src/main/java/com/piratariaprojetosweb/manager/controller/FuncionarioController.java
@@ -3,6 +3,7 @@ package com.piratariaprojetosweb.manager.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class FuncionarioController {
 	@Autowired
 	FuncionarioService service;
 	
+	@GetMapping
 	List<Funcionario> getAll(){
 		return service.getAll();
 	}
